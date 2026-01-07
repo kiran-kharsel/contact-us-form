82e1b82f8aee38ed8473694ee2089bddd02dda0c
kiran-kharsel <kirankharsel@gmail.com>
2026-01-07 09:22:50 +0530

    feat: add validation functionality for name and email

--- main.js
+++ main.js
@@ -14,3 +14,39 @@ formElem.addEventListener('submit', function(e){
     e.preventDefault();
     console.log('submitted')
 })
+
+
+// validation fn for name
+function validateName(name){
+    // This regex allows uppercase and lowercase letters, spaces, hyphens, and apostrophes
+    let pattern = /^[a-zA-Z\s'-]+$/;
+
+    if(name.length === 0){
+        console.log('name is required');
+        // error message on display
+    }else if(!pattern.test(name)){
+        console.log('Name contains invalid characters.');
+        // error message on display
+    } else {
+        return name;
+    }
+}
+
+
+// validation fn for email
+function validateEmail(email){
+    let pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(email.length === 0){
+        console.log('email is required');
+        // error message on display
+    }else if(!pattern.test(email)){
+        console.log('email contains invalid characters.');
+        // error message on display
+    } else {
+        console.log(email)
+        //return email;
+    }
+}
+
+validateEmail('can-3@gmail.com')
