deedc126f4f7cce8fccaef01dc0ed4e879c9bba3
kiran-kharsel <kirankharsel@gmail.com>
2026-01-07 08:46:05 +0530

    feat: get add dom elem

---
+++ main.js
@@ -0,0 +1,9 @@
+// dom elem
+const formElem = document.querySelector('.form-container');
+
+const firstName = formElem.querySelector('#first-name')
+const lastName = formElem.querySelector('#last-name')
+const email = formElem.querySelector('#email')
+const queries = formElem.querySelectorAll('.query')
+const message = formElem.querySelector('#message')
+const checkBox = formElem.querySelector('#checkbox')
