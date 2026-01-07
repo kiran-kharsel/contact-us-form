7fbea2def3d31e3b14132e9a73ef1d128405924c
kiran-kharsel <kirankharsel@gmail.com>
2026-01-07 08:48:29 +0530

    feat: add submit event to form elem

--- main.js
+++ main.js
@@ -7,3 +7,10 @@ const email = formElem.querySelector('#email')
 const queries = formElem.querySelectorAll('.query')
 const message = formElem.querySelector('#message')
 const checkBox = formElem.querySelector('#checkbox')
+
+
+
+formElem.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('submitted')
+})
